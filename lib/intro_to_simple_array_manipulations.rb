ba4905bb5ac75aacea35b3a1277df184b1e1de22
Soumya Goyal <somyagoyal739@gmail.com>
2019-09-04 17:32:28 +0000

    Done.

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,48 +1,48 @@
-def using_push(colors_in_the_rainbow, next_color)
-  colors_in_the_rainbow.push(next_color)
+def using_push(array, string)
+  array.push(string)
 end
 
-def using_unshift(bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc.unshift(new_neighborhood)
+def using_unshift(array, string)
+  array.unshift(string)
 end
 
-def using_pop(continents)
-  continents.pop
+def using_pop(array)
+  array.pop
 end
 
-def pop_with_args(dog_breeds)
-  dog_breeds.pop(2)
+def pop_with_args(array)
+  array.pop(2)
 end
 
-def  using_shift(my_favorite_cities)
-  my_favorite_cities.shift
+def  using_shift(array)
+  array.shift
 end
 
-def shift_with_args(ice_cream_brands)
-  ice_cream_brands.shift(2)
+def shift_with_args(array)
+  array.shift(2)
 end
 
-def using_concat(my_favorite_things, more_favs)
-  my_favorite_things.concat(more_favs)
+def using_concat(array1, array2)
+  array1.concat(array2)
 end
 
 
-def using_insert(list_of_programming_languages, another_language)
-  list_of_programming_languages.insert(4, another_language)
+def using_insert(array1, array2)
+  array1.insert(4, array2)
 end
 
-def using_uniq(haircuts)
-  haircuts.uniq
+def using_uniq(array)
+  array.uniq
 end
 
-def using_flatten(instruments)
-  instruments.flatten
+def using_flatten(array)
+  array.flatten
 end
 
-def using_delete(instructors, string)
-  instructors.delete(string)
+def using_delete(array, string)
+  array.delete(string)
 end
 
-def using_delete_at(famous_robots, number)
-  famous_robots.delete_at(number)
+def using_delete_at(array, number)
+  array.delete_at(number)
 end
